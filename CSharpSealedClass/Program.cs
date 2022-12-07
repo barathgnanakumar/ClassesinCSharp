e212ef0148eb512111841e1a2ee8cea8bab7726c
barathgnanakumar <gokulbarath31@gmail.com>
2022-12-07 12:53:11 +0530

    Add project files.

---
+++ CSharpSealedClass/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+namespace SealedClass
+{
+    sealed class Sealed
+    {
+        int a, b;
+        public void add(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+            int c = a + b;
+            Console.WriteLine("Sum of Two Numbers : " + c);
+        }
+    }
+    /*public class NewSeal : Sealed
+    {
+        int a, b;
+        public void multiply(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+            int c = a * b;
+            Console.WriteLine("Two Numbers Multiplied : " + c);
+        }
+    }*/
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Sealed seal = new Sealed();
+            seal.add(5, 3);
+       //   seal.multiply(5, 3);
+            Console.ReadKey();
+        }
+    }
+}
